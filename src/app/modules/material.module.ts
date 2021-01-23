87cb677384e2703b22efd1ccf6395d823a00985b
Pablo Thobias <pablo,thobias@gmail.com>
2021-01-23 12:58:47 -0300

    initial visual implementation applying responsivity

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 
 const materialComponentModules = [
     MatCardModule,
     MatDatepickerModule,
-    MatButtonModule
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatDividerModule
 ];
 
 @NgModule({
